6f80d1ca0b294f1782f7781f5b9901fa88dd3284
Inna Pokydko <inna.pokydko@gmail.com>
2023-06-17 15:56:24 +0300

    create functions

---
+++ contacts.js
@@ -0,0 +1,87 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+const { nanoid } = require("nanoid");
+
+// TODO: задокументувати кожну функцію
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading contacts file:", error);
+    return [];
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts.find((contact) => contact.id === contactId) || null;
+  } catch (error) {
+    console.error("Error", error);
+    return null;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contactToRemove = contacts.find(
+      (contact) => contact.id === contactId
+    );
+
+    if (!contactToRemove) {
+      return null;
+    }
+    const updatedContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(updatedContacts, null, 2),
+      "utf-8"
+    );
+
+    return contactToRemove;
+  } catch (error) {
+    console.error("Error", error);
+    return null;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+
+    contacts.push(newContact);
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(contacts, null, 2),
+      "utf-8"
+    );
+
+    return newContact;
+  } catch (error) {
+    console.error("Error", error);
+    return null;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
