d3a14f189c0e39f329c1ae325fedc83cedf4d058
Inna Pokydko <inna.pokydko@gmail.com>
2023-06-18 22:52:39 +0300

    update index.js with  commander

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-js-hw-01
+
+1. https://monosnap.com/file/k9W7A6srlSvcQz6JCmG2mYFRHD2PQ3
+
+2. https://monosnap.com/file/zErfQTReZdn5Pjw9ev4vlT51a1LPFn
+
+3. https://monosnap.com/file/pVslw283tnQrY6McVKh906hQ59yYGs
+
+4. https://monosnap.com/file/QmYW6Q3pzd1VlM7xkKr47rYFSJeu0M
--- contacts.js
+++ contacts.js
@@ -1,8 +1,8 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, ".", "db", "contacts.json");
-// const { nanoid } = require("nanoid");
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
 // TODO: задокументувати кожну функцію
 async function listContacts() {
@@ -15,76 +15,71 @@ async function listContacts() {
   }
 }
 
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts.find((contact) => contact.id === contactId) || null;
+  } catch (error) {
+    console.error("Error", error);
+    return null;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contactToRemove = contacts.find(
+      (contact) => contact.id === contactId
+    );
+
+    if (!contactToRemove) {
+      return null;
+    }
+    const updatedContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(updatedContacts, null, 2),
+      "utf-8"
+    );
+
+    return contactToRemove;
+  } catch (error) {
+    console.error("Error", error);
+    return null;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(contacts, null, 2),
+      "utf-8"
+    );
+    return newContact;
+  } catch (error) {
+    console.error("Error", error);
+    return null;
+  }
+}
 
 module.exports = {
   listContacts,
-  // getContactById,
-  // removeContact,
-  // addContact,
+  getContactById,
+  removeContact,
+  addContact,
 };
-
-// async function getContactById(contactId) {
-//   try {
-//     const data = await fs.readFile(contactsPath, "utf-8");
-//     const contacts = JSON.parse(data);
-//     return contacts.find((contact) => contact.id === contactId) || null;
-//   } catch (error) {
-//     console.error("Error", error);
-//     return null;
-//   }
-// }
-
-// async function removeContact(contactId) {
-//   try {
-//     const data = await fs.readFile(contactsPath, "utf-8");
-//     const contacts = JSON.parse(data);
-//     const contactToRemove = contacts.find(
-//       (contact) => contact.id === contactId
-//     );
-
-//     if (!contactToRemove) {
-//       return null;
-//     }
-//     const updatedContacts = contacts.filter(
-//       (contact) => contact.id !== contactId
-//     );
-//     await fs.writeFile(
-//       contactsPath,
-//       JSON.stringify(updatedContacts, null, 2),
-//       "utf-8"
-//     );
-
-//     return contactToRemove;
-//   } catch (error) {
-//     console.error("Error", error);
-//     return null;
-//   }
-// }
-
-// async function addContact(name, email, phone) {
-//   try {
-//     const data = await fs.readFile(contactsPath, "utf-8");
-//     const contacts = JSON.parse(data);
-
-//     const newContact = {
-//       id: nanoid(),
-//       name,
-//       email,
-//       phone,
-//     };
-
-//     contacts.push(newContact);
-//     await fs.writeFile(
-//       contactsPath,
-//       JSON.stringify(contacts, null, 2),
-//       "utf-8"
-//     );
-
-//     return newContact;
-//   } catch (error) {
-//     console.error("Error", error);
-//     return null;
-//   }
-// }
-
-
